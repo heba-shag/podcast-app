6ab6958d76a80cb837ed61d2b269cdb502bc0bf3
heba-shag <he.shag5007@gmail.com>
2025-06-21 22:51:25 +0300

    hashRouter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import { AuthProvider } from './Context/Auth-context';
 import i18n from './I18n';
 import { I18nextProvider } from 'react-i18next';
@@ -12,13 +12,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 i18n.init().then(() => {
   root.render(
     <React.StrictMode>
-      <BrowserRouter basename="/podcast-app">
+      <HashRouter basename="/podcast-app">
         <I18nextProvider i18n={i18n}>
           <AuthProvider>
             <App />
           </AuthProvider>
         </I18nextProvider>
-      </BrowserRouter>
+      </HashRouter>
     </React.StrictMode>
   );
 });
